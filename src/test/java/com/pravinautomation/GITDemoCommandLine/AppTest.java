7bd1665ffb1563b45c3ab0a4d46366f32ef5a0d3
Pravin Dhas <pravinsdhas@gmail.com>
2020-05-10 15:34:26 +0530

    first commit

---
+++ src/test/java/com/pravinautomation/GITDemoCommandLine/AppTest.java
@@ -0,0 +1,17 @@
+package com.pravinautomation.GITDemoCommandLine;
+
+import org.junit.Test;
+
+public class AppTest{
+	
+	@Test
+	public void Test1() {
+		System.out.println("test 1");
+	}
+	
+	@Test
+	public void Test2() {
+		System.out.println("test 2");
+	}
+	
+}
